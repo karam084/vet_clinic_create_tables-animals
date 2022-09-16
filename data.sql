b6c4bc6f155623bba116d627d7b8ee97fc40eaab
Karam Fouad <karam.ezz84@gmail.com>
2022-09-16 17:16:34 +0200

    Update data.sql
    Insert and update into animals

--- data.sql
+++ data.sql
@@ -5,3 +5,42 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	(2, 'Gabumon', '2018-11-15', 2, true, 8.00 ),
 	(3, 'Pikachu', '2021-01-07', 1, false, 15.04 ),
 	(4, 'Devimon', '2017-05-12', 5, true, 11.00 );
+	
+	INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
+  (5, 'Charmander', '2020-02-08', 0, false, -11),
+  (6, 'Plantmon', '2021-11-15',  2, true, -5.7),
+  (7, 'Squirtle', '1993-04-02',  3, false, -12.13),
+  (8, 'Angemon', '2005-06-12', 1, true, -45),
+  (9, 'Boarmon', '2005-06-07', 7, true, 20.4),    
+  (10, 'Blossom', '1998-10-13', 3, true, 17),
+  (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+begin;
+	update animals set species = 'unspecified';
+	rollback;
+        end;
+select * from animals;
+
+begin;
+	update animals set species = 'digimon' where name like '%mon';
+	update animals set species = 'pokemon' where species is null;
+	commit;
+        end;
+select * from animals;
+
+
+begin;
+	delete from animals;
+	rollback;
+        end;
+select * from animals;
+
+begin;
+	delete from animals where date_of_birth > '2022-01-01';
+	savepoint seperate_young_animals;
+	update animals set weight_kg = weight_kg * -1;
+	rollback to seperate_young_animals;
+	update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+	commit;
+         end;
+select * from animals;
