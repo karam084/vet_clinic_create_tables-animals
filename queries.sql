76e5e96849d8ca24eff5996684da3784379c9911
Karam Fouad <karam.ezz84@gmail.com>
2022-09-17 14:05:14 +0200

    Update queries.sql
    Queries into animals, vets, visits and specializations tables

--- queries.sql
+++ queries.sql
@@ -23,3 +23,44 @@ select animals.name, species.name, owners.full_name from animals join species on
 select animals.name, owners.full_name, animals.escape_attempts from animals join owners on animals.owner_id = owners.id where owners.full_name = 'Dean Winchester' and animals.escape_attempts = 0;
 select owners.full_name, count(animals.name) from animals right join owners on animals.owner_id = owners.id group by owners.full_name;
 
+select animals.name from animals
+join visits on visits.animals_id = animals.id
+join vets on visits.vets_id = vets.id
+where vets.name = 'William Tatcher'
+order by visits.visit_date desc
+limit 1;
+
+select count(animals.name) from animals
+join visits on visits.animals_id = animals.id
+join vets on visits.vets_id = vets.id
+where vets.name = 'Stephanie Mendez';
+select vets.name, species.name from vets
+FULL join specializations on specializations.vets_id = vets.id
+FULL join species on species.id = specializations.species_id;
+select animals.name from animals
+join visits on visits.animals_id = animals.id
+join vets on visits.vets_id = vets.id
+where vets.name = 'Stephanie Mendez' and visits.visit_date between '2020-04-01' and '2020-08-30';
+select animals.name, count(*) from animals
+join visits on visits.animals_id = animals.id
+group by animals.name
+order by count(*) desc
+limit 1;
+select animals.*, vets.*, visits.visit_date from animals
+join visits on visits.animals_id = animals.id
+join vets on visits.vets_id = vets.id
+order by visits.visit_date desc
+limit 1;
+select count(*) from visits
+FULL join animals on animals.id = visits.animals_id
+FULL join vets on visits.vets_id = vets.id
+FULL join specializations on specializations.vets_id = vets.id
+where specializations.species_id is null;
+select species.name, count(*) from visits
+join animals on animals.id = visits.animals_id
+join vets on visits.vets_id = vets.id
+join species on species.id = animals.species_id
+where vets.name = 'Maisy Smith'
+group by species.name
+order by count(*) desc;
+
