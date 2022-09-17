0f2c918653832174e1b0554244c32ee9a824a724
Karam Fouad <karam.ezz84@gmail.com>
2022-09-17 09:01:13 +0200

    Update schema.sql
    Create table owners and species

--- schema.sql
+++ schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE animals (
     PRIMARY KEY(id)
 );
 alter table animals add species text;
+
+create table owners (
+    id integer generated always as identity,
+    full_name text,
+    age integer,
+    primary key(id)
+);
+
+create table species (
+    id integer generated always as identity,
+    name text,
+    primary key(id)
+);
+
+alter table animals drop column species;
+alter table animals add column species_id integer;
+alter table animals add constraint FK_SPECIES foreign key (species_id) references species (id) on delete set null;
+
+alter table animals add column owner_id integer;
+alter table animals add constraint FK_OWNER foreign key (owner_id) references owners (id) on delete set null;
